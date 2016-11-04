6306a2cf8242e08930e9c326b656c04d2892d978
Taavi Ilp <taaviilp@ut.ee>
2016-11-04 22:37:51 +0200

    initial commit

---
+++ ut/systems/modelling/data/bpmn/BpmnModel.java
@@ -0,0 +1,7 @@
+package ut.systems.modelling.data.bpmn;
+
+/**
+ * Created by taavi on 11/3/2016.
+ */
+public class BpmnModel {
+}
---
+++ ut/systems/modelling/data/petrinet/PetriNetModel.java
@@ -0,0 +1,7 @@
+package ut.systems.modelling.data.petrinet;
+
+/**
+ * Created by taavi on 11/3/2016.
+ */
+public class PetriNetModel {
+}
